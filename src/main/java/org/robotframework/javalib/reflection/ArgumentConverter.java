4d00ae26a9f4cf9076e4825705bfd88cd81d2409
Gaurav Arora <gauravsworld@gmail.com>
2012-02-11 01:02:57 +0530

    Argument converter for string to * conversion

---
+++ src/main/java/org/robotframework/javalib/reflection/ArgumentConverter.java
@@ -0,0 +1,41 @@
+package org.robotframework.javalib.reflection;
+
+public class ArgumentConverter implements IArgumentConverter {
+
+    private final Class<?>[] parameterTypes;
+
+    public ArgumentConverter(Class<?>[] parameterTypes) {
+        this.parameterTypes = parameterTypes;
+    }
+
+    /** {@inheritDoc} */
+    public Object[] convertArguments(Object[] args) {
+        Object[] convertedArgs = new Object[args.length];
+        for (int i = 0; i < args.length; i++) {
+            if (!isArrayArgument(args[i])) {
+                convertedArgs[i] = convertToType(parameterTypes[i], args[i]);
+            } else {
+                convertedArgs[i] = args[i];
+            }
+        }
+        return convertedArgs;
+    }
+
+    private boolean isArrayArgument(Object object) {
+        return object.getClass().isArray();
+    }
+
+    private Object convertToType(Class<?> clazz, Object object) {
+        if (clazz == Integer.class) {
+            return Integer.valueOf(object.toString());
+        } else if (clazz == String.class) {
+            return object.toString();
+        } else if (clazz == Double.class) {
+            return Double.valueOf(object.toString());
+        } else if (clazz == Long.class) {
+            return Long.valueOf(object.toString());
+        }
+        return null;
+    }
+
+}
---
+++ src/main/java/org/robotframework/javalib/reflection/IArgumentConverter.java
@@ -0,0 +1,14 @@
+package org.robotframework.javalib.reflection;
+
+
+
+/**
+ * @author g.arora@iontrading.com
+ * @version $Id$
+ * @since 0.5.8
+ */
+public interface IArgumentConverter {
+
+    Object[] convertArguments(Object[] args);
+
+}

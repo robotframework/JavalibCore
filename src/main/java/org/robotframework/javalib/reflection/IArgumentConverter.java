8046d0b3dd8c3661194af0ab8e15e3769f5dd8d0
Gaurav Arora <gauravsworld@gmail.com>
2012-02-16 14:03:06 +0530

    remove class level javadoc

--- src/main/java/org/robotframework/javalib/reflection/IArgumentConverter.java
+++ src/main/java/org/robotframework/javalib/reflection/IArgumentConverter.java
@@ -2,11 +2,6 @@ package org.robotframework.javalib.reflection;
 
 
 
-/**
- * @author g.arora@iontrading.com
- * @version $Id$
- * @since 0.5.8
- */
 public interface IArgumentConverter {
 
     Object[] convertArguments(Object[] args);

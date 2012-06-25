433327ec02cc6e880940b7965e50d90acf6ce26e
Robot Framework Developers <robotframework@gmail.com>
2012-06-25 13:45:05 +0300

    Removed SpringLibrary and CompositeLibrary

--- src/test/java/org/robotframework/javalib/library/ClassLoadingIntegrationTest.java
+++ src/test/java/org/robotframework/javalib/library/ClassLoadingIntegrationTest.java
@@ -19,62 +19,8 @@ public class ClassLoadingIntegrationTest extends TestCase {
         assertClassWasLoaded("org.robotframework.javalib.keyword.EmptyKeyword");
     }
 
-    public void testSpringLibraryUsesProvidedClassLoaderForKeywordCreation() throws Exception {
-        SpringLibrary library = createSpringLibraryWithMockClassLoader();
-
-        library.runKeyword("Keyword Wired From Spring", null);
-        assertClassWasLoaded("org.robotframework.javalib.keyword.SpringKeyword");
-    }
-
-    public void testCompositeLibraryUsesProvidedClassLoaderForKeywordCreationWithASpringKeyword() throws Exception {
-        CompositeLibrary library = createCompositeLibraryWithMockClassLoader();
-
-        library.runKeyword("Keyword Wired From Spring", null);
-        assertClassWasLoaded("org.robotframework.javalib.keyword.SpringKeyword");
-    }
-
-    public void testCompositeLibraryUsesProvidedClassLoaderForKeywordCreationWithAClassPathKeyword() throws Exception {
-        CompositeLibrary library = createCompositeLibraryWithMockClassLoader();
-
-        library.runKeyword("Empty Keyword", null);
-        assertClassWasLoaded("org.robotframework.javalib.keyword.EmptyKeyword");
-    }
-
-    public void testCompositeLibraryUsesProvidedClassLoaderWithSpringKeywordFactory() throws Exception {
-        CompositeLibrary library = createCompositeLibraryWithMockClassLoader();
-        library.getKeywordNames();
-
-        CompositeKeywordFactory compositeKeywordFactory = (CompositeKeywordFactory) library.createKeywordFactory();
-        ApplicationContextKeywordFactory springKeywordFactory = (ApplicationContextKeywordFactory) compositeKeywordFactory.getKeywordFactories()[0];
-
-        ClassLoader usedClassLoader = springKeywordFactory.getClassLoader();
-        assertEquals(mockClassLoader, usedClassLoader);
-    }
-
-    public void testCompositeLibraryUsesProvidedClassLoaderWithClassPathKeywordFactory() throws Exception {
-        CompositeLibrary library = createCompositeLibraryWithMockClassLoader();
-        mockClassLoader.resetLists();
-
-        library.runKeyword("Empty Keyword", null);
-        assertClassWasLoaded("org.robotframework.javalib.keyword.EmptyKeyword");
-    }
-
     public void testClassPathLibraryUsesProvidedClassLoaderForKeywordExtraction() throws Exception {
         ClassPathLibrary library = createClassPathLibraryWithMockClassLoader();
-
-        library.getKeywordNames();
-        assertProvidedClassLoaderWasUsedForSearching();
-    }
-
-    public void testSpringLibraryUsesProvidedClassLoaderForKeywordExtraction() throws Exception {
-        SpringLibrary library = createSpringLibraryWithMockClassLoader();
-
-        library.getKeywordNames();
-        assertProvidedClassLoaderWasUsedForSearching();
-    }
-
-    public void testCompositeLibraryUsesProvidedClassLoaderForKeywordExtraction() throws Exception {
-        CompositeLibrary library = createCompositeLibraryWithMockClassLoader();
 
         library.getKeywordNames();
         assertProvidedClassLoaderWasUsedForSearching();
@@ -92,14 +38,6 @@ public class ClassLoadingIntegrationTest extends TestCase {
         assertTrue(mockClassLoader.loadedClasses.contains(expectedClassToBeLoaded));
     }
 
-    private CompositeLibrary createCompositeLibraryWithMockClassLoader() {
-        CompositeLibrary library = new CompositeLibrary();
-        library.setConfigFilePattern("org/**/keywords.xml");
-        library.setKeywordPattern("org/**/keyword/**.class");
-        library.setClassLoader(mockClassLoader);
-        return library;
-    }
-
     private ClassPathLibrary createClassPathLibraryWithMockClassLoader() {
         ClassPathLibrary library = new ClassPathLibrary();
         library.setKeywordPattern("org/**/keyword/**.class");
@@ -107,10 +45,4 @@ public class ClassLoadingIntegrationTest extends TestCase {
         return library;
     }
 
-    private SpringLibrary createSpringLibraryWithMockClassLoader() {
-        SpringLibrary library = new SpringLibrary();
-        library.setConfigFilePattern("org/**/keywords.xml");
-        library.setClassLoader(mockClassLoader);
-        return library;
-    }
 }

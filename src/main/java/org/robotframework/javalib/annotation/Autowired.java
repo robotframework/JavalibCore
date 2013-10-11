3bf9d2fd32ededdf57ba8ed2eaa6fe62a2a814fb
Jussi Malinen <jussi.ao.malinen@gmail.com>
2013-10-11 14:29:33 +0300

    added Markus Bernhardt as Copyright holder to autowired

--- src/main/java/org/robotframework/javalib/annotation/Autowired.java
+++ src/main/java/org/robotframework/javalib/annotation/Autowired.java
@@ -1,6 +1,7 @@
 /*
  * Copyright 2013 Nokia Solutions and Networks Oyj
- *  
+ * Copyright 2013 Markus Bernhardt
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
